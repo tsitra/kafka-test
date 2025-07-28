b38c71595dcfee35acb94f692f691d09f4da47b5
tsitra <nathan@nathanrbrown.dev>
2025-07-27 20:36:57 -0700

    Re-enabled Kafka stuff

--- listener/src/main/java/dev/nathanrbrown/kafka/test/TestApplication.java
+++ listener/src/main/java/dev/nathanrbrown/kafka/test/TestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
+//@EnableKafka
 @SpringBootApplication
 public class TestApplication {
 
--- listener/src/main/java/dev/nathanrbrown/kafka/test/kafka/TopicConfig.java
+++ listener/src/main/java/dev/nathanrbrown/kafka/test/kafka/TopicConfig.java
@@ -1,17 +1,24 @@
 package dev.nathanrbrown.kafka.test.kafka;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class TopicConfig {
     
-    // @Value(value = "${spring.kafka.bootstrap-servers}")
-    // private String bootstrapAddress;
+     @Value(value = "${spring.kafka.bootstrap-servers}")
+     private String bootstrapAddress;
 
-    // @Bean
-    // public KafkaAdmin kafkaAdmin() {
-        // Map<String, Object> configs = new HashMap<>();
-        // configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        // return new KafkaAdmin(configs);
-    // }
+     @Bean
+     public KafkaAdmin kafkaAdmin() {
+         Map<String, Object> configs = new HashMap<>();
+         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+         return new KafkaAdmin(configs);
+     }
 }
